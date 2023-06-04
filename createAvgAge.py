2dc274c2de7be482c17f313167fd3bc51a1fdd33
Gabriel <grodriguez@vbote.com>
2023-06-04 23:46:06 +0200

    Finished. Finally free

--- createAvgAge.py
+++ createAvgAge.py
@@ -4,19 +4,14 @@ lines = f.readlines()
 years = []
 for line in lines:
     if(line[:2]=="18"):
-        print("dieciocho")
         years.append(21)
     elif(line[:2]=="25"):
-        print("veinticinco")
         years.append(29.5)
     elif(line[:2]=="35"):
-        print("treintaycinco")
         years.append(39.5)
     elif(line[:2]=="45"):
-        print("cuarenta")
         years.append(49.5)
     elif(line[:2]=="55"):
-        print("cincuenta")
         years.append(59.5)
     else:
         print("error")
